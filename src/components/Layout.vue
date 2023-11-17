b96deb042c1f54690708d16361280dd61f0e0b40
salman <salmandriva@gmail.com>
2023-11-17 18:08:54 +0700

    menperbaiki link dari bootstrap icon

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,32 +1,113 @@
 <template>
-    <div class="app-container">
-      <div class="sidebar">
-        <!-- Sidebar content goes here -->
-        <router-link to="/">Home</router-link>
-        <router-link to="/customers">Customers</router-link>
-        <router-link to="/products">Products</router-link>
-      </div>
-  
-      <div class="content">
-        <!-- Main content goes here -->
+  <div class="container-fluid">
+    <div class="row">
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-light bg-light w-100">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <!-- Search Input -->
+            <form class="d-flex ms-auto">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+      <!-- Sidebar -->
+      <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar">
+        <div class="position-sticky">
+          <!-- Header -->
+          <div class="text-center mt-3">
+            <img v-if="user && user.photo" :src="`/images/${user.photo}`" alt="User Photo" class="rounded-circle" width="60" height="60" />
+            <div v-else class="rounded-circle placeholder-img" width="60" height="60"></div>
+            <p class="mt-2">{{ user ? user.email : 'Guest' }}</p>
+          </div>
+
+          <!-- Navigation Links -->
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">
+                <i class="bi bi-house-door"></i> Home
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/customers" class="nav-link">
+                <i class="bi bi-person"></i> Customers
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/products" class="nav-link">
+                <i class="bi bi-box"></i> Products
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <!-- Main content area -->
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <!-- Breadcrumb -->
+        <nav aria-label="breadcrumb" class="mt-3">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><router-link to="/">Home</router-link></li>
+            <li class="breadcrumb-item"><router-link to="/customers">Customers</router-link></li>
+            <li class="breadcrumb-item"><router-link to="/products">Products</router-link></li>
+            <li class="breadcrumb-item active" aria-current="page">{{ $route.name }}</li>
+          </ol>
+        </nav>
+
+        <!-- Router View -->
         <router-view></router-view>
-      </div>
+      </main>
     </div>
-  </template>
-  
-  <style scoped>
-  .app-container {
-    display: flex;
-  }
-  
-  .sidebar {
-    width: 200px;
-    background-color: #f0f0f0;
-    padding: 20px;
-  }
-  
-  .content {
-    flex: 1;
-    padding: 20px;
-  }
-  </style>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+  data() {
+    return {
+      // Contoh data pengguna, Anda dapat mengganti ini dengan informasi pengguna yang sebenarnya
+      user: {
+        email: 'user@example.com',
+        photo: '60.jpeg', // URL foto profil pengguna
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+#sidebar {
+  min-height: 100vh;
+}
+
+.nav-link {
+  display: flex;
+  align-items: center;
+  padding: 8px;
+  color: #333;
+}
+
+.bi {
+  margin-right: 8px;
+}
+
+/* Placeholder image style */
+.placeholder-img {
+  background-color: #ddd;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import "bootstrap/dist/css/bootstrap.min.css"
+
 import Layout from './components/Layout.vue'
 import router from './router';
 
 createApp(Layout)
 .use(router)
-.mount('#app')
+.mount('#app');
